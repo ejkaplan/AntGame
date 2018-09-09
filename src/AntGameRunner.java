56aea75676b740fd0865de5b676c487685b39b39
kapla <kapla@DESKTOP-TEKPELO>
2018-09-09 15:06:37 -0400

    remove references to non-existant bots

--- src/AntGameRunner.java
+++ src/AntGameRunner.java
@@ -2,33 +2,33 @@ import processing.core.PApplet;
 
 public class AntGameRunner extends PApplet {
 
-    public static void main(String[] args) {
-	PApplet.main("AntGameRunner");
-    }
+	public static void main(String[] args) {
+		PApplet.main("AntGameRunner");
+	}
 
-    private Game g;
-    private int rows = 50; // Change this to make the grid contain more or
-			   // fewer cells.
+	private Game g;
+	private int rows = 50; // Change this to make the grid contain more or
+	// fewer cells.
 
-    public void settings() {
-	// fullScreen();
-	size(3 * displayWidth / 4, 3 * displayHeight / 4);
-    }
+	public void settings() {
+		// fullScreen();
+		size(3 * displayWidth / 4, 3 * displayHeight / 4);
+	}
 
-    public void setup() {
-	frameRate(40); // Change this if you want the game to run slower to make
-		       // it easier to watch.
-	float squareWidth = 1f * height / rows;
-	int cols = round(width / squareWidth);
-	g = new Game(this, rows, cols);
-	// Your bots here:
-	new Locutus(g, "Locutus");
-	new RandomHive(g, "Random");
-    }
+	public void setup() {
+		frameRate(60); // Change this if you want the game to run slower to make
+		// it easier to watch.
+		float squareWidth = 1f * height / rows;
+		int cols = round(width / squareWidth);
+		g = new Game(this, rows, cols);
+		// Your bots here:
+		new RandomHive(g, "Random");
+		new RandomHive(g, "Random");
+	}
 
-    public void draw() {
-	background(255);
-	g.update();
-    }
+	public void draw() {
+		background(255);
+		g.update();
+	}
 
 }
